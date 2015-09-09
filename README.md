3df06989e04430a3db82bde5b2552a16777ee5c3
Balatzar <bataprod@gmail.com>
2015-09-09 08:39:53 +0200

    readme

---
+++ README.md
@@ -0,0 +1,9 @@
+## JS-PAINT
+
+Une petite app inutile pour apprendre le jquery (mais yen a très peu au final).
+
+C'est pas responsive, pas pratique, pas optimisé (du tout !) ect.
+
+Faites attention si vous clonez le code, le fichier index.html est très gros (plus de 27000 lignes).
+
+N'hésitez pas à forker le truc et à m'envoyer des pull requests !
--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
     var className = "k";
-    $('.color').click(function(event) {
+    
+    $('.color').click(function() {
         className = $(this).attr('class');
         console.log(className[0]);
    });
-    $('td').on('click', function(event) {
-        $('td').on('mouseover', function(event) {
+    $('td').on('click', function() {
+        $('td').on('mouseover', function() {
             $(this).toggleClass(className[0]);
         });
     });
