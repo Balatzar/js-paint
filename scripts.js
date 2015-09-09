538b5ada76b86b6cbd630c57d3439f4e76a89187
Balatzar <bataprod@gmail.com>
2015-09-09 09:09:45 +0200

    readme

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
     var className = "k";
     
+    $('.color').mouseenter(function(){
+        $(this).animate({color: '#0687f8'}, 'slow')
+    });
+    $('.color').mouseleave(function(){
+        $(this).animate({width: "50px", height: "50px", marginTop: "20px"}, 'slow')
+    });
+    
     $('.color').click(function() {
         className = $(this).attr('class');
         console.log(className[0]);
