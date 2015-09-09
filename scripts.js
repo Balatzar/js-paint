3ee3c2a15097a392626c6f875cf3a0b333f6ae50
Balatzar <bataprod@gmail.com>
2015-09-09 15:29:23 +0200

    swag h1

--- scripts.js
+++ scripts.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
     var className = "k";
     
-    $('.color').mouseenter(function(){
-        $(this).animate({color: '#0687f8'}, 'slow')
-    });
-    $('.color').mouseleave(function(){
-        $(this).animate({width: "50px", height: "50px", marginTop: "20px"}, 'slow')
-    });
-    
     $('.color').click(function() {
         className = $(this).attr('class');
-        console.log(className[0]);
    });
+    
+    
     $('td').on('click', function() {
         $('td').on('mouseover', function() {
             $(this).toggleClass(className[0]);
         });
     });
+    
+    $('.color').click(function() {
+        var color = $(this).css("background-color");
+        $('h1').replaceWith("<h1 style='color: " + color + ";'>Petit paint un peu nul</h1>");
+    });
+    
 });
